32999b31f69d3f0f4bc3d7e51909d8a216b02387
Nagendra Bajaj <nagendra.bajaj@gmail.com>
2024-01-10 18:48:26 -0500

    Inception

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", {id:"parent"},[
+    React.createElement("div", {id:"child"},
+                [React.createElement("h1",{id:"heading"},"I am h1 tag"),
+                React.createElement("h2",{id:"heading2"},"I am h2 tag")]),
+                React.createElement("div", {id:"child"},
+                [React.createElement("h1",{id:"heading"},"I am h1 tag"),
+                React.createElement("h2",{id:"heading2"},"I am h2 tag")])
+]);
+
+
+// const heading = React.createElement("h1",{id:"heading"},"Hello World from REACT!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
